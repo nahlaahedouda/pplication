6a0d9768bd46f77dccfbe2fa0d362b1ff11a5761
nadaouda <nada64165@gmail.com>
2023-03-04 10:51:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+class User{
+String id;
+String username;
+private  User(){}
+    User(String id, String username) {
+        this.id = id;
+        this.username = username;
+
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
